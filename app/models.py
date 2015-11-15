ba7550be5557eff5a88e3fb831424bba3a59eea0
osboxes.org <ashwynchadha@gmail.com>
2015-11-15 02:45:00 +0000

    Added concrete models, added views

--- app/models.py
+++ app/models.py
@@ -1,8 +1,37 @@
 from app import db
 
-class Test(db.Model):
+
+class Event(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  value = db.Column(db.Integer)
+  subject = db.Column(db.String(64))
+  start_date = db.Column(db.Date, index=True)
+  all_day = db.Column(db.Boolean)
+  start_time = db.Column(db.Time, index=True)
+  end_time = db.Column(db.Time)
+  location = db.Column(db.String(100))
+  description = db.Column(db.String(500))
+  class_id = db.Column(db.Integer, index=True)
 
   def __repr__(self):
-    return '<Test %r>' % self.value
+  	return "{} {} {} {} {} {} {} {}".format(self.subject, self.start_date.strftime("%m/%d/%y"), str(self.all_day), self.start_time.strftime("%I:%M %p"), self.end_time.strftime("%I:%M %p"), self.location, self.description, self.class_id) 
+
+  def to_json(self):
+    return {
+
+    'subject': self.subject,
+    'start_date': self.start_date.strftime("%m/%d/%y"),
+    'all_day': str(self.all_day),
+    'start_time': self.start_time.strftime("%I:%M %p"),
+    'end_time': self.end_time.strftime("%I:%M %p"),
+    'location': self.location,
+    'description': self.description,
+    'class_id': self.class_id
+
+    }
+
+class Class(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	class_id = db.Column(db.String(10), index=True, unique=True)
+	name = db.Column(db.String(100))
+
+  
--- app/views.py
+++ app/views.py
@@ -1,20 +1,46 @@
 from app import app, db, models
 import json
 import random
+import datetime
+
 
 @app.route('/')
 @app.route('/hello_world')
 def hello_world():
   return "Hello, world!"
 
-@app.route('/test/list')
+@app.route('/Event/list')
 def list():
-  return json.dumps(map(lambda x: x.value, models.Test.query.all()))
+  return json.dumps(map(lambda x: x.to_json(), models.Event.query.all()))
 
-
-@app.route('/test/add')
+@app.route('/Event/add')
 def add():
-  t = models.Test(value = random.randint(0, 100))
+  t = models.Event(subject = "TestEvent", start_date = datetime.date(2015, 10, 25), all_day=False, start_time=datetime.time(11, 0), end_time=datetime.time(11, 30), location="HiHi Home", description="This is a test case", class_id = "CS246")
   db.session.add(t)
   db.session.commit()
   return list()
+
+@app.route('/Event/deletefirst')
+def delete_first():
+	db.session.delete(models.Event.query.get(1))
+	db.session.commit()
+	return list()
+
+@app.route('/Event/deleteall')
+def delete_all():
+	for E in models.Event.query.all():
+		db.session.delete(E)
+	db.session.commit()
+	return list()
+
+@app.route('/Event/Query')
+def Qry():
+	return json.dumps(map(lambda x: x.to_json(), models.Event.query.filter_by(class_id = 'CS243').order_by(models.Event.start_date).all()))
+
+@app.route('/Event/add5')
+def add5():
+	for i in range(5):	
+  		t = models.Event(subject = "TestEvent", start_date = datetime.date(2015, 10, i+1), all_day=False, start_time=datetime.time(11, 0), end_time=datetime.time(11, 30), location="HiHi Home", description="This is a test case", class_id = "CS24{}".format(str(i)))
+		db.session.add(t)
+  	db.session.commit()
+  	return list()
